970897f3f7c68cb4673420e2352ed36071414db4
James Brooks <brooksjamesk01@gmail.com>
2022-02-10 18:19:57 -0800

    replaced question 7 with fixed a fixed verion of the  original question 7 that failed

--- js/app.js
+++ js/app.js
@@ -179,6 +179,7 @@ function numberQuestion() {
   }
 }
 
+// eslint-disable-next-line no-unused-vars
 function petQuestion() {
   let guesses = 6;
   let answeredCorrectly = false;
@@ -199,6 +200,28 @@ function petQuestion() {
   }
 }
 
+function movieQuestion() {
+  let favoiteMovives = ['20,000 leagues under the sea', '12 angry men', 'titanic', 'avengers infinity war', 'inherit the wind', 'trading places', 'the princess bride', 'three billboards outside ebbing, missouri', 'operation odessa', 'wall-e'];
+  let guesses = 6;
+  let answeredCorrectly = false;
+  //console.log(guesses);
+  while (guesses > 0 && answeredCorrectly === false) {
+    guesses--;
+    let answer8 = prompt('what is one of my favorite movies?');
+    for (let i = 0; i <= favoiteMovives.length; i++) {
+      let movieName = favoiteMovives[i];
+      if (movieName === answer8) {
+        alert('Correct, that is one of them');
+        answeredCorrectly = true;
+      }
+    }
+    //console.log(guesses);
+    if (guesses === 0) {
+      alert(`You have run out of attempts. The possible answers were ${favoiteMovives}`);
+    }
+  }
+}
+
 // eslint-disable-next-line no-unused-vars
 function questionButton() {
   if (counter >= 7) {
@@ -211,7 +234,8 @@ function questionButton() {
   timingQuestion();
   lifeQuestion();
   numberQuestion();
-  petQuestion();
+  //petQuestion();
+  movieQuestion();
   //console.log('Number of Correct Answers ' + counter)
   endResponse();
 }
